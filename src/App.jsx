36f72c476adee2cce800830d545ef2bcb25936b2
Jason Christian <jasonchristian1234@gmail.com>
2026-06-04 02:44:37 +1200

    feat: add BlogContainer.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import Header from "./ui_components/Header";
 import NavBar from "./ui_components/NavBar";
+import BlockContainer from "./ui_components/BlogContainer";
 
 const App = () => {
   return (
     <>
       <div>
         <NavBar />
+        <Header />
+        <BlockContainer />
       </div>
     </>
   );
--- src/ui_components/CardFooter.jsx
+++ src/ui_components/CardFooter.jsx
@@ -1,29 +1,25 @@
-import { BASE_URL } from "@/api";
-import { Link } from "react-router-dom";
 import pic from "../images/pic.jpg";
 
-const CardFooter = ({ blog }) => {
+const CardFooter = () => {
   return (
-    <Link to={`/profile/${blog.author.username}`}>
-      <div className="flex items-center gap=4 ">
-        <span className="flex items-center gap-2">
-          <div className="w-[40px] h-[40px] rounded-full overflow-hidden">
-            <img
-              src={pic}
-              className="c rounded-full w-full h-full object-cover"
-            />
-          </div>
+    <div className="flex items-center gap=4 ">
+      <span className="flex items-center gap-2">
+        <div className="w-[40px] h-[40px] rounded-full overflow-hidden">
+          <img
+            src={pic}
+            className="c rounded-full w-full h-full object-cover"
+          />
+        </div>
 
-          <small className="text-[#97989F] text-[12px] font-semibold">
-            John Doe
-          </small>
-        </span>
-
-        <small className="text-[#97989F] text-[12px] font-semibold ml-3">
-          04 Jun 2026
+        <small className="text-[#97989F] text-[12px] font-semibold">
+          John Doe
         </small>
-      </div>
-    </Link>
+      </span>
+
+      <small className="text-[#97989F] text-[12px] font-semibold ml-3">
+        04 Jun 2026
+      </small>
+    </div>
   );
 };
 
