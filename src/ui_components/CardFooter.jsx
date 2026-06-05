dcb9fcbb89298a3e3a8991a6c8777bb7e1f98d43
Jason Christian <jasonchristian1234@gmail.com>
2026-06-06 05:31:40 +1200

    feat: display date

--- src/ui_components/CardFooter.jsx
+++ src/ui_components/CardFooter.jsx
@@ -1,4 +1,5 @@
 import { BASE_URL } from "@/api";
+import { FormatDate } from "@/services/formatDate";
 
 const CardFooter = ({ blog }) => {
   return (
@@ -17,7 +18,7 @@ const CardFooter = ({ blog }) => {
       </span>
 
       <small className="text-[#97989F] text-[12px] font-semibold ml-3">
-        04 Jun 2026
+        {FormatDate(blog.published_date)}
       </small>
     </div>
   );
